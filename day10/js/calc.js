a5f1872002133d637e830997043fb325da286f05
xxxxxseed <xxxxxseed@gmail.com>
2022-05-20 18:18:38 +0900

    자바스크립트 소스 day10

---
+++ day10/js/calc.js
@@ -0,0 +1,12 @@
+// 덧셈 프로그램
+let n1 = parseInt(prompt("첫번째 수 입력 : "));
+let n2 = parseInt(prompt("두번째 수 입력 : "));
+let sum_v = 0;
+
+// 연산
+// sum_v = parseInt(n1) + parseInt(n2);
+sum_v = n1 + n2;
+
+
+// 출력
+document.write("두 수의 합 : " + sum_v);
