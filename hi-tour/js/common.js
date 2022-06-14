f7f82eaf561fafc610bae739b6d738a10d978128
xxxxxseed <xxxxxseed@gmail.com>
2022-06-14 18:23:28 +0900

    hi-tour modified

--- hi-tour/js/common.js
+++ hi-tour/js/common.js
@@ -1,15 +1,24 @@
 
 
 $(document).ready( function() {
-	
+	// let isOver1 = false; // 상위메뉴(마우스오버되면: true/)
+	// let isOver2 = false; // 하위메뉴(마우스오버되면: true/)
+
+	let isOver1 = [false, false, false, false]; // 0, 1, 2, 3
+	let isOver2 = [false, false, false, false];
+
+
 	// fadeOut()의 시간 설정
 	function goHide(){
 		for(var i = 1; i <= 4; i++){
-			$(".gnb_depth2_" + i).fadeOut("fast");
+			if(!isOver1[i] && !isOver2[0]){ // 상위, 하위 메뉴가 닿았을때
+			$(".gnb_depth2_" + (i+1)).fadeOut("fast");
+			}
 		}
 	}
 	
 	// 상단 메뉴 마우스오버, 탭 클릭 코드
+	// 상위메뉴
 	$(".openAll1").mouseover(function(){
 		// 서브메뉴 나타나기(header의 너비가 800보다 클때)
 		if(parseInt($("header").css("width")) > 800)
@@ -17,12 +26,16 @@ $(document).ready( function() {
 	}).focus(function(){
 		if(parseInt($("header").css("width")) > 800)
 			$(".gnb_depth2_1").fadeIn("fast");
+			isOver1[0] = true;
 	}).mouseout(function(){
 		// $(".gnb_depth2_1").fadeOut("fast");
-		setTimeout(goHide, 500); // 0.5초
+		setTimeout(goHide, 300); // 0.5초
+			isOver1[0] = true;
 	}).blur(function(){
-		setTimeout(goHide, 500);
+		setTimeout(goHide, 300);
+			isOver1[0] = false;
 	});
+	// 하위메뉴
 	
 	$(".openAll2").mouseover(function(){
 		// 서브메뉴 나타나기(header의 너비가 800보다 클때)
@@ -33,9 +46,9 @@ $(document).ready( function() {
 			$(".gnb_depth2_2").fadeIn("fast");
 	}).mouseout(function(){
 		// $(".gnb_depth2_2").fadeOut("fast");
-		setTimeout(goHide, 500);
+		setTimeout(goHide, 300);
 	}).blur(function(){
-		setTimeout(goHide, 500);
+		setTimeout(goHide, 300);
 	});
 	
 	$(".openAll3").mouseover(function(){
@@ -47,9 +60,9 @@ $(document).ready( function() {
 			$(".gnb_depth2_3").fadeIn("fast");
 	}).mouseout(function(){
 		// $(".gnb_depth2_3").fadeOut("fast");
-		setTimeout(goHide, 500);
+		setTimeout(goHide, 300);
 	}).blur(function(){
-		setTimeout(goHide, 500);
+		setTimeout(goHide, 300);
 	});
 	
 	$(".openAll4").mouseover(function(){
@@ -61,9 +74,9 @@ $(document).ready( function() {
 			$(".gnb_depth2_4").fadeIn("fast");
 	}).mouseout(function(){
 		// $(".gnb_depth2_4").fadeOut("fast");
-		setTimeout(goHide, 500);
+		setTimeout(goHide, 300);
 	}).blur(function(){
-		setTimeout(goHide, 500);
+		setTimeout(goHide, 300);
 	});
 	
 	// top을 클릭하면 header로 이동(라이브러리)
--- hi-tour/js/rollmain.js
+++ hi-tour/js/rollmain.js
@@ -1,62 +1,66 @@
+// 이미지 슬라이드 구현
 
- 	var wnum=0;
-	var wwslidingAuto=null;
-	function play_w(directw) {	
-			if(directw=="right"){
-				wnum++;
-				if(wnum>2)wnum=0;
-			}else if(directw=="left"){
-				wnum--;
-				if(wnum<0)wnum=2;
-			}else {
-				wnum = directw;
-			}
-			
-			$(".rollingbtn").find('li.seq a').each(function(){
-				$(".rollingbtn li.seq a img").attr('src',$(".rollingbtn li.seq a img").attr('src').replace('_on.png','_off.png'));
-			});
-			$(".rollingbtn li.butt"+wnum+ " a img").attr('src',$(".rollingbtn li.butt"+wnum+ " a img").attr('src').replace('_off.png','_on.png'));
+let num = 0; // 이미지 번호
+let slidingAuto = null; // 슬라이드 작동 (객체이므로 null)
 
-				if(wnum==0) {
-				$(".viewImgList li.imglist1").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist2").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist0").animate({'opacity' : 1 },1000);
-			}else if (wnum==1){
-				$(".viewImgList li.imglist0").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist2").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist1").animate({'opacity' : 1 },1000);
-			}else if (wnum==2){
-				$(".viewImgList li.imglist0").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist1").animate({'opacity' : 0 },1000);
-				$(".viewImgList li.imglist2").animate({'opacity' : 1 },1000);
-			}
-
-			if(wwslidingAuto)clearTimeout(wwslidingAuto);
-			wwslidingAuto=setTimeout("play_w('right')",3000);
+function play(direct){ // 방향 매개 변수 설정
+	if(direct == "right"){
+		num++;
+		if(num > 2)
+			num = 0;
+	}else if(direct == "left"){
+		num--;
+		if(num < 0)
+			num = 2;
+	}else{
+		num = direct;
 	}
-	var wwslidingAuto=setTimeout("play_w('right')",3000);
 
-$(document).ready(function(){
+	// 이미지 애니메이션
+	if(num == 0){
+		$(".viewImgList li.imglist1").animate({"opacity" : 0}, 1000);
+		$(".viewImgList li.imglist2").animate({"opacity" : 0}, 1000);
+		$(".viewImgList li.imglist0").animate({"opacity" : 1}, 1000);
+	}else if(num == 1){
+		$(".viewImgList li.imglist0").animate({"opacity" : 0}, 1000);
+		$(".viewImgList li.imglist2").animate({"opacity" : 0}, 1000);
+		$(".viewImgList li.imglist1").animate({"opacity" : 1}, 1000);
+	}else if(num == 2){
+		$(".viewImgList li.imglist0").animate({"opacity" : 0}, 1000);
+		$(".viewImgList li.imglist1").animate({"opacity" : 0}, 1000);
+		$(".viewImgList li.imglist2").animate({"opacity" : 1}, 1000);
+	}
 
+	// 각각의 버튼이 on -> off로 이동, 계속 전체적으로 순환
+	$(".rollingbtn").find("li.seq a").each(function(){
+		$(".rollingbtn li.seq a img").attr("src",
+			$(".rollingbtn li.seq a img").attr("src").replace("_on.png", "_off.png"));
+	});
+
+	// 하나의 버튼이 on -> off로 변경
+	$(".rollingbtn li.butt" + num + " a img").attr("src",
+			$(".rollingbtn li.butt" + num + " a img").attr("src").replace("_off.png", "_on.png"));
+
+	slidingAuto = setTimeout("play('right')", 3000); // 콜백
+}
+
+// 슬라이드 3초 간격으로 작동
+slidingAuto = setTimeout("play('right')", 3000);
+
+// 재생과 정지 구현
+$(function(){
+	// 정지 버튼을 클릭하면
 	$(".rollstop a").click(function(){
-		$(this).parent().hide();
-		$(".rollplay").css('display','inline-block');
-		if(wwslidingAuto)clearTimeout(wwslidingAuto);	
+		$(this).parent().hide(); // 정지버튼 숨기기
+		$(".rollplay").css("display", "inline-block"); // 재생버튼 보임
+		if(slidingAuto) // 슬라이드가 작동중이면
+			clearTimeout(slidingAuto); // 정지시킴
 	});
 
+	// 재생 버튼을 클릭하면
 	$(".rollplay a").click(function(){
-		$(this).parent().hide();
-		$(".rollstop").css('display','inline-block');
-		play_w('right');
+		$(this).parent().hide(); // 재생버튼 숨기기
+		$(".rollstop").css("display", "inline-block"); // 정지버튼 보임
+		play("right");
 	});
-
-	$(".rollingbtn li.seq a").each(function(index){
-		$(this).click(function(){
-			$(".rollplay").hide();
-			$(".rollstop").css('display','inline-block');
-			if(wwslidingAuto)clearTimeout(wwslidingAuto);	
-			play_w(index);
-		});
-	});
-
 });
